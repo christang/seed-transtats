ab872d85ce0e16aec406b2d5fb16666377c4d8b0
Chris Tang <chris.l.tang@gmail.com>
2015-06-09 16:59:49 -0400

    Add fetch_nws.py
    (Nat'l Weather Service, Adv. Hydrologic Pred. Service)

---
+++ dat/ahps/fetch_nws.py
@@ -0,0 +1,15 @@
+from datetime import timedelta, date
+import wget
+
+def daterange(start_date, end_date):
+    for n in range(int ((end_date - start_date).days)):
+        yield start_date + timedelta(n)
+
+start_date = date(2011, 1, 1)
+end_date = date(2012, 12, 31)
+
+for current in daterange(start_date, end_date):
+    current = {'day': current.day, 'month': current.month, 'year': current.year}
+    url = 'http://water.weather.gov/precip/p_download_new/%(year)d/%(month)02d/%(day)02d/' % current
+    url += 'nws_precip_%(year)d%(month)02d%(day)02d_nc.tar.gz' % current
+    wget.download(url)
